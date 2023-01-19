f2d9b38d237119fbd71778357392a97a37d08d47
cuiqimeng <cuiqm3@sunacwy.com.cn>
2023-01-19 16:19:18 +0800

    chore: 版本号

--- 不要到处CoCo.js
+++ 不要到处CoCo.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         不要到处coco
 // @namespace    https://wydevops.coding.net/
-// @version      0.8
+// @version      0.9
 // @description  不潮不用花钱
 // @author       你
 // @match        https://wydevops.coding.net/*
